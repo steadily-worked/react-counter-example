8138af5fcc830055c5bb949ccb12ccb5e34539ec
steadily-worked <qpwpep5429@gmail.com>
2022-05-15 17:14:00 +0900

    apply template

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Counter.css';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  const handlePlusCount = () => {
-    setCount(count + 1);
-  };
-  const handleMinusCount = () => {
-    setCount(count - 1);
-  };
-
+  const count = 0;
   return (
     <div className="Container">
       <div className="Title">현재 숫자는 {count}입니다. 😎</div>
       <div className="ButtonContainer">
-        <button className="Button" onClick={handlePlusCount}>
-          +1
-        </button>
-        <button className="Button" onClick={handleMinusCount}>
-          -1
-        </button>
+        <button className="Button">+1</button>
+        <button className="Button">-1</button>
       </div>
     </div>
   );
